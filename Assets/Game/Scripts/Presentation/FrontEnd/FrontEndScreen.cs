ee3d05d60fb4bf685186c585f0d9fb9a21a24f83
rickwyatt234 <rickwyatt234@gmail.com>
2026-09-22 14:48:56 -0400

    Add front-end screen enum

---
+++ Assets/Game/Scripts/Presentation/FrontEnd/FrontEndScreen.cs
@@ -0,0 +1,15 @@
+namespace RaceFatal.Presentation.FrontEnd
+{
+    public enum FrontEndScreen
+    {
+        Boot,
+        PressAnyInput,
+        MainMenu,
+        CampaignSelect,
+        TeamCreation,
+        RacerCreation,
+        CampaignReview,
+        Options,
+        Credits
+    }
+}
